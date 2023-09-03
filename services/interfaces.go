bd7e9a883319399652fb1b1d5ce5a4759aa0be59
leeden <denis.li@payme.uz>
2023-09-03 19:05:36 +0500

    [*] Updated interfaces

--- services/interfaces.go
+++ services/interfaces.go
@@ -1,15 +1,14 @@
 package services
 
 import (
-	"github.com/Shemistan/Lesson_6/api/dtos"
 	"github.com/Shemistan/Lesson_6/storage/models"
 )
 
 type IService interface {
-	Auth(req dtos.AuthRequest) (int, error)
-	UpdateUser(req dtos.UpdateUserRequest) error
-	GetUser(req dtos.GetUserRequest) (models.User, error)
-	GetUsers(req dtos.GetUsersRequest) ([]models.User, error)
-	DeleteUser(req dtos.DeleteUserRequest) error
-	GetStatistics(req dtos.GetStatisticsRequest) models.Statistic
+	Auth(req models.User) (int, error)
+	UpdateUser(id int, user models.User) error
+	GetUser(id int) (models.User, error)
+	GetUsers() ([]models.User, error)
+	DeleteUser(id int) error
+	GetStatistics() models.Statistic
 }
