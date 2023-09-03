2b9842b6e378a2cd87c88f1e087fbc06f7bf0ee5
leeden <denis.li@payme.uz>
2023-09-03 18:15:33 +0500

    [+] Implemented API/Service interface

---
+++ api/interfaces.go
@@ -0,0 +1,12 @@
+package api
+
+import "github.com/Shemistan/Lesson_6/api/dtos"
+
+type IApi interface {
+	Auth(req dtos.AuthRequest) (dtos.AuthResponse, error)
+	UpdateUser(req dtos.UpdateUserRequest) (dtos.UpdateUserResponse, error)
+	GetUser(req dtos.GetUserRequest) (dtos.GetUserResponse, error)
+	GetUsers(req dtos.GetUsersRequest) (dtos.GetUsersResponse, error)
+	DeleteUser(req dtos.DeleteUserRequest) error
+	GetStatistics(req dtos.GetStatisticsRequest) (dtos.GetStatisticsResponse, error)
+}
