b05d9a80ebef9770d029eaaba5c0f072b379f550
leeden <denis.li@payme.uz>
2023-09-03 19:07:47 +0500

    [*] Updated interfaces

--- services/interfaces.go
+++ services/interfaces.go
@@ -5,8 +5,8 @@ import (
 )
 
 type IService interface {
-	Auth(req models.User) (int, error)
-	UpdateUser(id int, user models.User) error
+	Auth(user *models.User) (int, error)
+	UpdateUser(id int, user *models.User) error
 	GetUser(id int) (models.User, error)
 	GetUsers() ([]models.User, error)
 	DeleteUser(id int) error
