1e88b4e53867c3b95d9d387a68ed1267d0c0dd06
leeden <denis.li@payme.uz>
2023-09-03 18:37:29 +0500

    [+] Added hashing password

--- helpers/converters/auth_to_user_model.go
+++ helpers/converters/auth_to_user_model.go
@@ -1,6 +1,8 @@
 package converters
 
 import (
+	"crypto/sha256"
+	"encoding/hex"
 	"github.com/Shemistan/Lesson_6/api/dtos"
 	"github.com/Shemistan/Lesson_6/storage/models"
 )
@@ -8,8 +10,16 @@ import (
 func AuthToUserModel(dto dtos.AuthRequest) models.User {
 	return models.User{
 		Name:     dto.Login,
-		Password: dto.Password,
+		Password: hashPassword(dto.Password),
 		Status:   models.StatusActive,
 		Role:     models.RoleUser,
 	}
+}
+
+func hashPassword(password string) string {
+	hash := sha256.New()
+	hash.Write([]byte(password))
+	hashB := hash.Sum(nil)
+
+	return hex.EncodeToString(hashB)
 }
