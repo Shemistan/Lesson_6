d862641dd5608f98d9be9c8f9e024ead360d0237
nodirbeksulaymonov <s.nodir9494@gmail.com>
2023-09-26 22:24:10 +0500

    refork

---
+++ cmd/main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"golang-api/internal/api"
+	"golang-api/internal/models"
+	"golang-api/internal/service"
+	"golang-api/internal/storage"
+)
+
+func main() {
+	conn := storage.NewConnect()
+	db := storage.New("localhost", 5432, 30, conn)
+	serv := service.New(db)
+	handler := api.New(serv)
+	Server(handler)
+}
+
+func Server(handler api.IApi) {
+	 _, err := handler.Auth(&models.AddRequest{
+		AuthParams: models.AuthData{
+			Login: "nods1",
+			Password: "nods123",
+		},
+	})
+
+	if err != nil {
+		log.Println(err.Error())
+	}
+
+	_, err = handler.Auth(&models.AddRequest{
+		AuthParams: models.AuthData{
+			Login: "nods2",
+			Password: "nods123",
+		},
+	})
+
+	if err != nil {
+		log.Println(err.Error())
+	}
+
+	updateUser := handler.UpdateUser(1, &models.UserDate{
+		Name: "Nods",
+		Surname: "Sulaymonov",
+		Status: "online",
+		Role: "user",
+		RegistrationDate: time.Time{},
+		UpdateDate: time.Time{},
+	})
+
+	if updateUser != nil {
+		fmt.Println(updateUser)
+		return
+	}
+
+	_, err = handler.GetUser(1)
+
+	if err != nil {
+		log.Println(err.Error())
+	} 
+
+	err = handler.DeleteUser(1)
+
+	if err != nil {
+		log.Println(err)
+	} 
+
+	_, err = handler.GetUsers()
+
+	if err != nil {
+		log.Println(err.Error())
+	} 
+
+	handler.GetStatistics()
+}
---
+++ internal/api/api_test.go
@@ -0,0 +1,130 @@
+package api
+
+import (
+	"errors"
+	"testing"
+	"time"
+
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/assert"
+
+	"golang-api/internal/models"
+	"golang-api/internal/service"
+	mock_storage "golang-api/internal/storage/mocks"
+)
+
+func TestService(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	storage := mock_storage.NewMockIStorage(ctrl)
+	serv := service.New(storage)
+	api := New(serv)
+
+	t.Run("Get all users", func(t *testing.T) {
+		storage.EXPECT().GetUsers().Return([]*models.User{}, nil)
+
+		userList, err := api.GetUsers()
+
+		if err != nil {
+			t.Error(err)
+		}
+
+		assert.Equal(t, []*models.User{}, userList)
+
+	})
+
+	userLogin := &models.AddRequest {
+		AuthParams: models.AuthData{
+			Login: "nods",
+			Password: "dasmkdmak12231",
+		},
+	}
+
+	t.Run("Auth return error ", func(t *testing.T) {
+		storage.EXPECT().Auth(gomock.Any()).Return(0, errors.New("some errors"))
+
+		_, err := api.Auth(userLogin)
+
+		assert.Equal(t, errors.New("some errors"), err)
+
+	})
+
+	t.Run("Auth return success ", func(t *testing.T) {
+		storage.EXPECT().Auth(gomock.Any()).Return(1, nil)
+
+
+		res, _ := api.Auth(userLogin)
+
+		assert.Equal(t, 1, res)
+
+	})
+
+	t.Run("GetUser return error ", func(t *testing.T) {
+		storage.EXPECT().GetUser(gomock.Any()).Return(&models.User{}, errors.New("some errors"))
+
+		_, err := api.GetUser(0)
+
+		assert.Equal(t, errors.New("some errors"), err)
+
+	})
+
+	t.Run("GetUser return success ", func(t *testing.T) {
+
+		testUser := models.User{
+			Id:               1,
+			Login:            "nods",
+			HashPassword:     "dmsafmsa2f3",
+			Name:             "Nodir",
+			Surname:          "Sulaymonov",
+			Status:           "active",
+			Role:             "user",
+			RegistrationDate: time.Time{},
+			UpdateDate:       time.Time{},
+		}
+
+		storage.EXPECT().GetUser(gomock.Any()).Return(&testUser, nil)
+
+		res, _ := api.GetUser(1)
+
+		assert.Equal(t, &testUser, res)
+
+	})
+
+	t.Run("UpdateUser return error ", func(t *testing.T) {
+		storage.EXPECT().UpdateUser(gomock.Any(), gomock.Any()).Return(errors.New("some errors"))
+
+		res := api.UpdateUser(0, nil)
+
+		assert.Equal(t, errors.New("some errors"), res)
+
+	})
+
+	t.Run("UpdateUser return success ", func(t *testing.T) {
+		storage.EXPECT().UpdateUser(gomock.Any(), gomock.Any()).Return(nil)
+
+		res := api.UpdateUser(1, nil)
+
+		assert.Equal(t, nil, res)
+
+	})
+
+	t.Run("DeleteUser return error", func(t *testing.T) {
+		storage.EXPECT().DeleteUser(gomock.Any()).Return(errors.New("some errors"))
+
+		err := api.DeleteUser(0)
+
+		assert.Equal(t, errors.New("some errors"), err)
+	})
+
+	t.Run("DeleteUser return success", func(t *testing.T) {
+		storage.EXPECT().DeleteUser(gomock.Any()).Return(nil)
+
+		err := api.DeleteUser(1)
+
+		assert.Equal(t, nil, err)
+	})
+
+
+}
+	
